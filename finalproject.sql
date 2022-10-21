b995b8753ad214d98091926a3d1cec6e34af78fe
Carissa McCormick <101649525+crsamc@users.noreply.github.com>
2022-10-21 14:49:54 -0400

    Update finalproject.sql

--- finalproject.sql
+++ finalproject.sql
@@ -10,12 +10,16 @@ CREATE TABLE performance (
 	 writing_score INT NOT NULL
 );
 
+SELECT * FROM performance
+
 CREATE TABLE race (
      race_ethnicity VARCHAR(20) NOT NULL,
 	 math_score INT NOT NULL,
 	 reading_score INT NOT NULL,
 	 writing_score INT NOT NULL
 );
+
+SELECT * FROM race
 
 CREATE TABLE parentedu (
      parental_education VARCHAR(20) NOT NULL,
@@ -24,6 +28,8 @@ CREATE TABLE parentedu (
 	 writing_score INT NOT NULL
 );
 
+SELECT * FROM parentedu
+
 CREATE TABLE lunchstat (
      lunch VARCHAR(20) NOT NULL,
 	 math_score INT NOT NULL,
@@ -31,9 +37,13 @@ CREATE TABLE lunchstat (
 	 writing_score INT NOT NULL
 );
 
+SELECT * FROM lunchstat
+
 CREATE TABLE testprep (
      test_prep VARCHAR(20) NOT NULL,
 	 math_score INT NOT NULL,
 	 reading_score INT NOT NULL,
 	 writing_score INT NOT NULL
 );
+
+SELECT * FROM testprep
